de03a3c764b92233891ed6e1422d3a9781c84e81
MinhTruc <conankaitou007@gmail.com>
2019-09-30 16:27:35 +0700

    test comit

---
+++ BenhNhan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class BenhNhan
+    {
+        public string maso { get; set; }
+        public string socm { get; set; }
+        public string hoten { get; set; }
+        public string diachi { get; set; }
+    }
+}
